81590c18a63212fb29534b8b3833c56fcf53d23e
FeriPrasetiyo22 <ferytio40@gmail.com>
2022-10-18 22:47:09 +0700

    db

--- app.js
+++ app.js
@@ -3,48 +3,64 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'Localhost',
-  database: 'breaddb',
-  password: '12345',
-  port: 5432,
-})
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-var indexRouter = require('./routes/index')(pool);
-var usersRouter = require('./routes/users')
+// Database Name
+const dbName = 'users';
 
-var app = express();
+async function main() {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    //const collection = db.collection('users');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    // the following code examples can be pasted here...
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    var indexRouter = require('./routes/index')(db);
+    var usersRouter = require('./routes/users')(db);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    var app = express();
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
 
-module.exports = app;
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    module.exports = app;
+  } catch (err) {
+    throw "failed to connect"
+  }
+}
+
